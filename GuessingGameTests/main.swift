d0f9f44a04c670d2f12405d6e59a6e9dab169ab2
Ben-G <Benjamin.Encz@gmail.com>
2015-11-18 20:09:45 -0800

    initial commit

---
+++ GuessingGameTests/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GuessingGameTests
+//
+//  Created by Benjamin Encz on 11/18/15.
+//  Copyright © 2015 Make School. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
